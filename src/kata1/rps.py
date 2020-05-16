d0bf99c61bc6a7ee8eb43d603fdac8d1dd290354
Javier <saldeaqui@gmail.com>
2020-05-16 20:28:38 +0200

    Kata1 code

--- src/kata1/rps.py
+++ src/kata1/rps.py
@@ -7,17 +7,33 @@ options = ["Piedra", "Papel", "Tijeras"]
 #'Ganaste!'
 #'Perdiste!'
 def quienGana(player, ai):
-    return ""
-
+    resultado=""
+    if player == 'Piedra' and ai == 'Papel' or player == 'Papel' and ai == 'Piedra':
+        resultado = "Perdiste!"
+    elif player == ia:
+        resultado = "Empate!"        
+    elif player == 'Papel' and ai == 'Tijeras' or player == 'Tijeras' and ai == 'Papel':
+        resultado = "Perdiste!"
+    elif player == 'Piedra'and ai == 'Tijeras':
+        resultado = "Ganaste!"
+    elif player == 'Papel' and ai == 'Piedra':
+        resultado = "Ganaste!"
+    elif player == 'Tijeras' and ai == 'Papel':
+        resultado = "Ganaste!"
+    return resultado
+    
 # Entry Point
 def Game():
     #
     #
-    
-    #
+    player=input("Eliga una opcion: ")
+    indice=random.randint(0, len(options))
+    ai=options[indice]
     #
     
     winner = quienGana(player, ai)
 
     print(winner)
+    
+Game()
 
