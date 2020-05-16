f5510644cc94c8019649c87bf5fb1f7694e28e9b
Javier <saldeaqui@gmail.com>
2020-05-16 20:29:28 +0200

    kata dos solucion

--- src/kata2/rpg.py
+++ src/kata2/rpg.py
@@ -6,8 +6,14 @@ import string
 def RandomPasswordGenerator(passLen=10):
     #
     #
+    letters = string.ascii_lowercase
     
+    
+    return ''.join(random.choice(letters) for i in range(passLen)) 
     #
     #
 
-    return ""
+    
+
+password=RandomPasswordGenerator()
+print(password)
