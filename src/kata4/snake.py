2764e1d964146a5ec00f8775f00bc4bf2a4c6d71
Javier <saldeaqui@gmail.com>
2020-05-23 19:43:56 +0200

    change errors

--- src/kata4/snake.py
+++ src/kata4/snake.py
@@ -14,28 +14,14 @@ class Snake():
     # Manejo del pressed [KEYDOWN] de las teclas [K_RIGHT - K_LEFT - K_UP -K_DOWN ]
     def controller(self, event, pygame):                
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:                  
-                    for l in self.body:                        
-                        l[0] += 1                
-                        l[1] += 1
-                elif event.key == pygame.K_LEFT:
-                    for l in self.body:
-                        l[0] -= 1
-                        l[1] -= 1                    
-                elif event.key == pygame.K_UP:
-                    for l in self.body:
-                        l[0] -= 1
-                        l[1] -= 1
-                elif event.key == pygame.K_DOWN:
-                    for l in self.body:
-                        l[0] -= 1
-                        l[1] -= 1
-                    
-
-            elif event.type == pygame.KEYUP:
-                for l in self.body:
-                    l[0] += 0
-                    l[1] += 0
+                if event.key == pygame.K_RIGHT:
+                    self.change = "RIGHT"
+                if event.key == pygame.K_LEFT:
+                    self.change = "LEFT"
+                if event.key == pygame.K_UP:
+                    self.change = "UP"
+                if event.key == pygame.K_DOWN:
+                    self.change = "DOWN"
                 
 
     # Controla el cambio de  las direcciones
@@ -45,19 +31,23 @@ class Snake():
     # Incremento del movimiento 
     def changeDirection(self):
 
-        for event in pygame.event.get():
-            if event.type == pygame.K_RIGTH:
-                self.position[0]+=1
-                self.position[1]+=1
-            if event.type == pygame.K_LEFT:
-                self.position[0]-=1
-                self.position[1]-=1
-            if pygame.event.type == pygame.K_UP:
-                self.position[0]+=1
-                self.position[1]+=1
-            if pygame.event.type == pygame.K_DOWN:
-                self.position[0]-=1
-                self.position[1]-=1
+        if self.change == "RIGHT" and self.direction != "LEFT":
+            self.direction = "RIGHT"
+        if self.change == "LEFT" and self.direction != "RIGHT":
+            self.direction = "LEFT"
+        if self.change == "UP" and self.direction != "DOWN":
+            self.direction = "UP"
+        if self.change == "DOWN" and self.direction != "UP":
+            self.direction = "DOWN"
+
+        if self.direction == "RIGHT":
+            self.position[0] += 10
+        if self.direction == "LEFT":
+            self.position[0] -= 10
+        if self.direction == "UP":
+            self.position[1] -= 10
+        if self.direction == "DOWN":
+            self.position[1] += 10
 
         self.body.insert(0, list(self.position))
 
@@ -72,28 +62,23 @@ class Game():
     # función de salida
     def exit(self, event, pygame):
         if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+            self.run = False
+
         
     
     # Posición aleatorio entre el ranto [0,49] * 10  
     def food_spawn(self):
-        self.food_pos = 0
+        self.food_pos = [random.randint(0,49)*10, random.randint(0,49)*10]
 
     # Si colisionas con una fruta, sumas 1
     # Sino decrementas en 1 el body del snake
     def eat(self, snake):
         
-        if snake.body == self.food_pos:             
-            for l in snake.body:
-                l[0] += 1
-                l[1] += 1
-            
-                
+        if snake.position == self.food_pos:
+            self.food_spawn()
+            self.score += 1
         else:
-            for l in snake.body:
-                l[0] -= 1
-                l[1] -= 1
+            snake.body.pop()
             
                 
         #    
@@ -106,16 +91,18 @@ class Game():
     # Posición snake[1] >= 500 ó snake[1] <= 0                  -> Muere
     # Posición del snake choca con sigo mismo menos la cabeza   -> Muere 
     def dead(self, snake):
-        if snake[0] > 499 or snake[0] < 1:
-            for l in snake.body:
-                l[0] += 1
-                l[1] += 1
+        if snake.position[0] > 499 or snake.position[0] < 1:
+            print(f"Game Over! Score: {self.score})")
+            self.run = False
             
-        if snake[0] > 499 or snake[1] < 1:
-            for l in snake.body:
-                l[0] = 0
-                l[1] = 0
-            
+        if snake.position[1] >= 500 or snake.position[1] <=0:
+            print(f"Game Over! Score: {self.score})")
+            self.run = False
+
+        if snake.position in snake.body[1:]:
+            print(f"Game Over! Score: {self.score})")
+            self.run = False
+
                     
         #
         #
